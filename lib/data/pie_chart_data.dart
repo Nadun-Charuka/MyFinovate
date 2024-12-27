f81602798d940bda8d00479bbec860f92dfd2b46
Nadun-Charuka <nadun.charuka2016@gmail.com>
2024-12-27 21:43:30 +0530

    ok

--- lib/data/pie_chart_data.dart
+++ lib/data/pie_chart_data.dart
@@ -1,11 +1,10 @@
-import 'package:fitness_tracker_v90/constants/colors.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
 class PieChartSampleData {
   final pieChartSectionData = [
     PieChartSectionData(
-      color: primaryColor,
+      color: const Color(0xff2697ff),
       value: 30,
       showTitle: false,
       radius: 14,
