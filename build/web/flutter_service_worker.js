1ab6d0c32daea6c198d8af44a4d72e1bd76b1f09
Nadun-Charuka <nadun.charuka2016@gmail.com>
2024-12-28 15:23:16 +0530

    final commit

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -31,7 +31,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "0f553f7d38d36c8b4f160b687eb19d0f
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "aba94748b73816339ad0ccc68be24b42",
+"flutter_bootstrap.js": "8a28c72a67ceda7041aa7bbde517d5d4",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
